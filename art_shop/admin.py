831352ae997001455c2b205959f61a4e5c82552e
vaibhavkhatik821 <99136077+vaibhavkhatik@users.noreply.github.com>
2025-04-12 07:22:16 +0530

    Initial commit

---
+++ art_shop/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product,About,Contact,Registration1,My_art,AddToCart,Orders
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(About)
+admin.site.register(Contact)
+admin.site.register(My_art)
+admin.site.register(AddToCart)
+admin.site.register(Orders)
+admin.site.register(Registration1)
+
